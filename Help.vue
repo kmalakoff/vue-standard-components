80af50fb5f0f44bf5e279e7f26e43625c3f48ca4
Ran <ran.guin@gmail.com>
2017-08-17 12:14:30 -0700

    add Help & Modal components

---
+++ Help.vue
@@ -0,0 +1,54 @@
+<!-- src/components/Help.vue -->
+
+  <template lang='pug'>
+    div
+      Modal(v-if="showModal" @close="showModal=false" :modalBody="message")
+      div(v-else)
+        button.btn.btn-success(id="show-modal" @click="showModal = true")
+          div(v-if="demoStep")
+            b {{demoStep}}
+          div(v-else)
+            b ?
+
+  </template>
+
+  <script>
+  import Modal from './Modal'
+
+  export default {
+    components: {
+      Modal
+    },
+    data () {
+      return {
+        step: 1,
+        showModal: false
+      }
+    },
+    computed: {
+      id: function () {
+        if (this.demoStep) {
+          return 'demoStep' + this.demoStep
+        } else {
+          return this.name
+        }
+      }
+    },
+    props: {
+      demoStep: {
+        type: Number
+      },
+      name: {
+        type: String,
+        default: 'help'
+      },
+      message: {
+        type: String
+      }
+    }
+  }
+  </script>
+
+<style>
+  
+</style>
