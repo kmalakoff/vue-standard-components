d76445e35cd2108283f4e320361d6a51bf54f0c6
Ran Guin <ran.guin@gmail.com>
2017-08-28 20:27:11 -0700

    simplify passage of modal options; add link options; add Usage

--- ActionButton.vue
+++ ActionButton.vue
@@ -4,18 +4,32 @@
 
 <template lang='pug'>
   div
-    div(v-if='modalButton')
+    div(v-if='modal')
       div(v-if="showModal")
-        Modal(v-if="showModal" :table='modalTable' type='append' @close="hideM" :modalAction="runModalEvent" :modalButton="modalButton" :modalBody="modalBody" :modalFooter="modalFooter")
+        Modal(v-if="showModal" @close="hideM" :record="record" :name="name" :table="modal.table" :button="modal.button" :body="modal.body" :function="modal.function" :url="modal.url" :urlData="modal.urlData")
       div(v-else)
-        button.btn.btn-success(id="show-modal" @click.prevent="showM") {{buttonName}}
+        button.btn.btn-success(id="show-modal" @click.prevent="showM") {{name}}
     div(v-else)
-      button.btn.btn-success(@click.prevent="runEvent()") {{buttonName}}
+      button.btn.btn-success(@click.prevent="runEvent()") {{name}}
 </template>
 
 <script>
 import Modal from './../Standard/Modal.vue'
+/*
 
+Usage:
+
+  ActionButton(:name="link.name" :type="link.type" :modal="link.modal" :record="data[index]" :link="link")
+
+Input:
+
+  name - name of modal (should be distinct if multiple modals used)
+  type - button / modal
+
+Advanced Options:
+
+  record - data record to pass to modal for secondary action
+*/
 export default {
   name: 'immunize',
   data () {
@@ -28,29 +42,14 @@ export default {
     Modal
   },
   props: {
-    buttonName: {
+    name: {
       type: String
     },
-    buttonAction: {
-      type: Function
+    modal: {
+      type: Object
     },
-    modalTable: {
-      type: String
-    },
-    modalButton: {
-      type: String
-    },
-    modalAction: {
-      type: Function
-    },
-    modalHeader: {
-      type: String
-    },
-    modalBody: {
-      type: String
-    },
-    modalFooter: {
-      type: String
+    link: {
+      type: Object
     },
     record: {
       type: Object
@@ -69,11 +68,11 @@ export default {
       this.showModal = false
     },
     runEvent () {
-      console.log('run ' + this.buttonName)
+      console.log('run event ' + JSON.stringify(this.link))
       console.log('data: ' + JSON.stringify(this.record))
-      if (this.buttonAction) {
-        console.log('Action: ' + this.buttonAction.constructor)
-        this.buttonAction({ record: this.record })
+      if (this.link.function) {
+        console.log('Action: ' + this.link.function.constructor)
+        this.link.function({ record: this.record })
       }
       console.log(JSON.stringify(this.record))
       return false
