f5aa64c421557aee32c19e90300ea046bcb99017
Ran Guin <ran.guin@gmail.com>
2017-11-12 11:51:06 -0800

    tweak myState default

--- setup/src/components/myState.js
+++ setup/src/components/myState.js
@@ -5,33 +5,37 @@ Vue.use(Vuex)
 
 // import StdState from './../Standard/StdState'
 // const ovid = {
+// use Messaging component to enable real time error messaging ...
 
 export default new Vuex.Store({
   state: {
     user: {},
-
+    count: 1,
     messageContexts: [],
     errors: {},
     errorCount: 0,
     logs: []
   },
   mutations: {
+    increment: state => state.count++,
+    decrement: state => state.count--,
     clearUser (state) {
       console.log('clear ' + state.user.name)
       state.user.id = null
       state.user.name = null
     },
 
-    setError (state, data) {
+    setError (state, data, clear) {
       var context = data.context || 'default'
 
       console.log('Error ' + data.context)
       console.log('set Error: ' + data.err)
 
-      if (this.messageContexts.indexOf(context) === -1) {
-        this.messageContexts.push(context)
+      if (Object.keys(state.errors).indexOf(context) === -1 || data.clear) {
+        state.messageContexts.push(context)
         state.errors[context] = []
       }
+
       if (data.err) {
         if (data.err.constructor === Error && data.err.message) {
           state.errors[context].push(data.err.message)
@@ -40,6 +44,7 @@ export default new Vuex.Store({
         }
       }
       state.errorCount++
+      console.log(state.errorCount + ' ERRORS')
     },
     clearErrors (state, context) {
       console.log('clear errors...')
