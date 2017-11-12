49b9d5217be8ebfcfee904b2c2e723d53048cf4b
Ran Guin <ran.guin@gmail.com>
2017-11-12 14:49:46 -0800

    update default page

---
+++ setup/src/components/PublicFooter.vue
@@ -0,0 +1,44 @@
+<template lang='pug'>
+  div.publicFooter
+    div.footerContent
+      h4 Public Footer (Default)
+        div.navbar-right
+          b final tag..
+</template>
+
+
+<script>
+export default {
+  // template: require('./main.pug'),
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+  .publicFooter {
+    padding: 0px;
+    background-color: #ccc;
+  }
+
+  .footerContent {
+    padding: 10px;
+  }
+
+  h1, h2 {
+    font-weight: normal;
+  }
+
+  ul {
+    list-style-type: none;
+    padding: 0;
+  }
+
+  li {
+    display: inline-block;
+    margin: 0 10px;
+  }
+
+  a {
+    color: #42b983;
+  }
+</style>
