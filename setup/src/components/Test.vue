3484f271e668097758a1f0d7f263d5db02789181
Ran Guin <ran.guin@gmail.com>
2017-12-11 16:39:25 -0800

    add test component

---
+++ setup/src/components/Test.vue
@@ -0,0 +1,23 @@
+<template lang='pug'>
+  div.container
+    h3 Test
+    hr
+    DBForm(table="vaccine")
+</template>
+
+<script>
+
+import DBForm from './Standard/DBForm'
+
+export default {
+  name: 'test',
+  components: {
+    DBForm
+  },
+  data () {
+    return {
+      msg: 'Welcome to My Vue.js App'
+    }
+  }
+}
+</script>
