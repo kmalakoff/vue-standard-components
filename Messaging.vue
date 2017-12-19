80e510d786a2a82c1902e9419c858aea62b83298
Ran Guin <ran.guin@gmail.com>
2017-12-18 17:57:53 -0800

    add warnings & messages

--- Messaging.vue
+++ Messaging.vue
@@ -24,16 +24,35 @@
 
 <template lang='pug'>
   div.message-block
-    div(v-if='errorCount')
+    div(v-if='errorCount || warningCount || messageCount')
       div(v-for='context,key in errors' align='center')
-        h4 {{errors[key].length}} {{key} Error(s) detected:
-        div.table.msg-errors.alert-danger(v-if="errors[key].length" align='center')
-          tr
-            td
-              div(v-for='error in errors[key]')
-                b.msg-errors {{error}}
-            td
-              button(@click.prevent="clear(key)") x
+        div(v-if="errors[key].length")
+          h4 {{errors[key].length}} {{key} Error(s) detected:
+          div.table.msg-errors.alert-danger(align='center')
+            tr
+              td
+                div(v-for='error in errors[key]')
+                  b.msg-errors {{error}}
+              td
+                button(@click.prevent="clear(key)") x
+        div(v-if="warnings[key].length")
+          h4 {{warnings[key].length}} {{key} Warnings(s) found:
+          div.table.msg-warnings.alert-warning(align='center')
+            tr
+              td
+                div(v-for='warning in warnings[key]')
+                  b.msg-warnings {{warnings}}
+              td
+                button(@click.prevent="clear(key)") x
+        div(v-if="messages[key].length")
+          h4 {{messages[key].length}} {{key}} Message(s):
+          div.table.msg-messages.alert-success(align='center')
+            tr
+              td
+                div(v-for='message in messages[key]')
+                  b.msg-messages {{message}}
+              td
+                button(@click.prevent="clear(key)") x
 </template>
 
 <script>
@@ -59,7 +78,11 @@
     computed: mapState([
       'count',
       'errorCount',
-      'errors'
+      'errors',
+      'warningCount',
+      'warnings',
+      'messageCount',
+      'messages'
     ]),
     methods: {
       clear (scope) {
@@ -74,8 +97,7 @@
   .message-block {
     /*border: 1px solid black*/
   }
-
-  .msg-errors {
+  .msg-errors, .msg-warnings, .msg-messages {
     padding: 10px;
     margin: 10px;
     width: 50%;
