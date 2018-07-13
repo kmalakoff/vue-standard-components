f5949fc59f09f145891465fff05309566f2fa073
Ran Guin <ran.guin@gmail.com>
2018-07-13 16:03:42 -0700

    add modal for search results

--- SearchButton.vue
+++ SearchButton.vue
@@ -4,7 +4,7 @@
       span.input-group
         span.input-group-addon(style='background-color: transparent; border: 0px;')
           a(href='/' onclick='return false' @click.prevent="toggleSearch")
-            icon(name='search' color='white' scale='2')
+            icon(name='search' color='blue' scale='2')
           span &nbsp; &nbsp;
     div(@mouseleave="hideSearch")
       table.table.popup-table.input-lg(v-if="visibleSearch")
@@ -15,15 +15,22 @@
                 input.form-control.input-lg.full-page#searchString(type='text' placeholder='-- Search --')
                 span.input-group-btn
                   button.btn.btn-lg.btn-default.full-page(@click.prevent='searchMethod')
-                    icon(name='search' color='black' background='blue')
+                    icon(name='search' color='red' background='blue')
+    Modal(id='searchresults' type='data')
+      span &nbsp; &nbsp;
+
 </template>
 
 <script>
   // require icon supplied by calling component...
-  // import Modal from './Modal'
+  import Modal from './Modal'
   import 'vue-awesome/icons/search'
 
   export default {
+    name: 'SearchButton',
+    components: {
+      Modal
+    },
     data () {
       return {
         holdSearch: false,
@@ -66,8 +73,49 @@
           console.log('no search string...')
         }
         console.log('perform search for ' + search)
+
+        var results = [
+          { name: 'searched for', value: search },
+          { name: 'found_eg', value: 'value_eg' }
+        ]
+
+        this.$store.dispatch('clearModal')
+        // populate modal...
+        this.$store.dispatch('setModalData', results)
+        this.$store.dispatch('toggleModal', 'searchresults')
+        console.log('results: ' + JSON.stringify(results))
       }
     }
   }
 </script>
+
+<style scoped lang="sass?outputStyle=expanded">
+
+ .popup-table {
+    position: absolute;
+    right: 0;
+    top: 60px;
+    margin: 0px;
+  }
+  .popup-table tr {
+    /*padding: 5px;*/
+  }
+  .popup-table tr td {
+    /*padding: 0px;*/
+    /*padding-left: 15px;*/
+  }
+
+  .popup-table a{
+    display: block;
+    padding: 5px;
+    padding-left: 15px;
+    /*text-decoration: none;*/
+  }
+  .popup-table a:hover{
+    display: block;
+    padding: 5px;
+    padding-left: 15px;
+    text-decoration: none;
+  }
+</style>
 
