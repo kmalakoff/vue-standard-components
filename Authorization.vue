5fcf8c590535db3cc13e88dcaf6fbc81d6301c6a
Ran Guin <ran.guin@gmail.com>
2018-09-05 17:48:36 -0700

    add Authorization and DropdownMenu components

---
+++ Authorization.vue
@@ -0,0 +1,226 @@
+<!-- src/components/Authorization.vue -->
+
+<template lang='pug'>
+  div
+    <!-- b V:  -->
+    <!-- a (href='#' @click.prevent='profile') Profile -->
+    div(v-if='userid')
+      DropdownMenu(:options='userMenu' :title='payload.username')
+      <!-- span {{payload.username}} -->
+      <!-- span &nbsp; | &nbsp; -->
+      <!-- a(href='#' @click.prevent='logout') -->
+        <!-- b Log Out -->
+    div(v-else)
+      Modal(type='record' id='login-modal' :options='loginOptions' :note='note')
+      span &nbsp; | &nbsp;
+      Modal(type='record' id='register-modal' :options='registerOptions' :note='note')
+        p &nbsp;
+</template>
+<script>
+import Modal from './Modal'
+import DropdownMenu from './DropdownMenu'
+import auth from '../../auth'
+export default {
+  data () {
+    return {
+      // We need to initialize the component with any
+      // properties that will be used in it
+      demo: true,
+      credentials: {
+        username: '',
+        password: ''
+      },
+      error: '',
+      note: 'asldfj',
+
+      userMenu: [
+        { label: 'Profile', loadModal: this.getUser },
+        { label: 'Logout', loadModal: this.logout }
+      ],
+      loginOptions: {
+        openText: 'Log in',
+        access: 'append',
+        fields: [
+          {name: 'email', type: 'text'},
+          {name: 'password', placeholder: 'password', type: 'password'}
+        ],
+        onSave: this.login,
+        onBlur: this.checkInput,
+        onFocus: this.inputFocus,
+        submitButton: 'Log Me In'
+      },
+
+      registerOptions: {
+        openText: 'Sign up',
+        access: 'append',
+        fields: [
+          {name: 'username'},
+          {name: 'email', type: 'text'},
+          {name: 'password', placeholder: 'password', type: 'password'},
+          {name: 'confirmPassword', placeholder: 're-enter password', type: 'password'}
+        ],
+        onSave: this.register,
+        onBlur: this.checkInput,
+        onFocus: this.inputFocus,
+        submitButton: 'Register'
+      }
+    }
+  },
+  components: {
+    Modal,
+    DropdownMenu
+  },
+  props: {
+    payload: {
+      type: Object
+    },
+    onPick: { type: Function }
+  },
+  computed: {
+    // payload: function () {
+    //   var P = localStorage.getItem('payload')
+    //   return P
+    // },
+    userid: function () {
+      if (this.payload && this.payload.userid) {
+        return this.payload.userid
+      } else {
+        return null
+      }
+    }
+  },
+  methods: {
+    profile () {
+      console.log('verify current profile')
+    },
+    register (form) {
+      var credentials = {
+        username: form.username,
+        email: form.email,
+        password: form.password,
+        confirmPassword: form.confirmPassword
+      }
+
+      console.log('Registering: ' + JSON.stringify(credentials))
+      auth.signup(this, credentials, 'secretquote')
+    },
+    login (form) {
+      var credentials = {
+        email: form.email,
+        password: form.password
+      }
+      console.log('Authorizing for login: ' + JSON.stringify(credentials))
+
+      var P = auth.login(this, credentials, 'secretquote')
+      var payload = localStorage.getItem('payload')
+
+      console.log('Payload: ' + JSON.stringify(payload))
+      console.log('auth.login: ' + JSON.stringify(P))
+
+      this.$store.dispatch('payload', payload)
+
+      // console.log('is ' + this.$store.getters.payload)
+
+      // var user = payload.user
+      // console.log(user + ' Payload: ' + JSON.stringify(payload))
+
+      // var access = auth.checkAuth()
+      // console.log('access: ' + access)
+
+      // var payload2 = auth.payload()
+      // console.log(JSON.stringify(payload2))
+
+      // var token = localStorage.getItem('id_token')
+      // console.log('token: ' + token)
+    },
+    logout () {
+      this.$store.dispatch('AUTH_LOGOUT')
+    },
+    getUser () {
+      var data = JSON.parse(this.$store.getters.payload)
+      var keys = Object.keys(data)
+
+      var D = []
+      for (var i = 0; i < keys.length; i++) {
+        D.push({attribute: keys[i], value: data[keys[i]]})
+      }
+      console.log('user data: ' + JSON.stringify(D))
+      return D
+    },
+    makeAuth (e) {
+      // auth logic
+      this.note = 'Register failed'
+    },
+    checkInput (e) {
+      console.log('validate input')
+      // const form = document.querySelector('.form')
+      const parent = e.target.parentElement
+      console.log('found ')
+      parent.classList.add('has-error')
+      this.note = 'Failed validation'
+    },
+    inputFocus (e) {
+      this.note = ''
+      const parent = e.target.parentElement
+      parent.classList.remove('has-error')
+      console.log('checkinput')
+    },
+    inputValidate (e) {
+      this.note = ''
+      const parent = e.target.parentElement
+      parent.classList.add('has-success')
+      console.log('validated')
+    }
+  }
+  // watch: {
+  //   note () {
+  //     console.log('note changed...')
+  //     const note = document.querySelector('.note')
+  //     if (this.note.length) {
+  //       note.classList.add('note--up')
+  //     } else {
+  //       note.classList.remove('note--up')
+  //       note.classList.add('note--down')
+  //     }
+  //   }
+  // }
+}
+</script>
+
+<style scoped>
+
+.login-box {
+  padding: 1rem;
+  border: solid black 1px;
+  text-align: center;
+}
+.note {
+  // color: red;
+  background: #FF9E80;
+  padding: 0.75rem 1.5rem;
+  box-sizing: border-box;
+  position: relative;
+  bottom: 100%;
+  z-index: 0;
+  width: 100%;
+  transition: all .5s ease-out;
+}
+.note-mask {
+  // background: #FF9E80;
+  background: #FFFFFF;
+  padding: 0.75rem 1.5rem;
+  box-sizing: border-box;
+  position: relative;
+  bottom: 100%;
+  z-index: 0;
+  transition: all .5s ease-out;
+}
+.note--down {
+  transform: translateY(100%);
+}
+
+.note--up {
+  transform: translateY(0);
+}
+
+</style>
---
+++ DropdownMenu.vue
@@ -0,0 +1,200 @@
+<!-- src/components/Standard/Menu.vue
+
+  Usage:
+
+  used to generate dropdown options from menu - use to trigger a modal containing data or a method execution
+
+  Modes of operation:
+
+  eg DropdownMenu(:title='username' :options='options')
+
+  this.options = [
+    {label: 'Profile', loadModal: 'getUserData'},
+    {label: 'Logout', onClick: 'logout'}
+  ]
+-->
+
+<template lang='pug'>
+  div.customMenu
+    a(href='/' onclick='return false' v-on:mouseover="showMenu" v-on:mouseout="hideMenu(1)" v-on:click='toggleMenu')
+      span.menu-title
+        icon(v-if="visibleMenu" name='caret-up')
+        icon(v-if="!visibleMenu" name='caret-down')
+        span &nbsp; {{title}} &nbsp;
+    div
+      table.table.popup-table.input-lg(v-if="visibleMenu" v-on:mouseover="showMenu" v-on:mouseout="hideMenu(1)")
+        tr(v-for="option in options" width='100%')
+          td.option-label(width='100%')
+            a(href='/' onclick='return false' @click.prevent="runMethod(option)")
+              span.option-label {{option.label}}
+    Modal(v-if='modal' :id='modal' type='data')
+      span &nbsp; &nbsp;
+
+</template>
+
+<script>
+// require icon supplied by calling component...
+import Modal from './Modal'
+import 'vue-awesome/icons/search'
+import 'vue-awesome/icons/caret-up'
+import 'vue-awesome/icons/caret-down'
+
+export default {
+  name: 'DropdownMenu',
+  components: {
+    Modal
+  },
+  data () {
+    return {
+      visibleMenu: false,
+      activeOption: '',
+
+      searchIcon: {
+        type: String,
+        default: 'search'
+      },
+      showAfterWait: false,
+      waitingToClose: false,
+      keepOn: false
+      // options: [
+      //   {label: 'Profile', loadModal: this.getUser},
+      //   {label: 'Logout', onClick: this.logout}
+      // ]
+    }
+  },
+  props: {
+    title: {
+      type: String,
+      default: '?'
+    },
+    options: { type: Array }, // list of dropdown items (hashes with keys for: title, onClick, tooltip)
+    modal: { type: String } // modal id (optional)
+  },
+  computed: {
+  },
+  methods: {
+    getUser (e) {
+      console.log('getUser: ' + JSON.stringify(e))
+      var id = document.getElementById('searchString')
+      var search = ''
+      if (id && id.value) {
+        console.log('search for: ' + id.value)
+        search = id.value
+      } else {
+        console.log('no search string...')
+      }
+      console.log('perform search for ' + search)
+
+      var results = [
+        { name: 'searched for', value: search },
+        { name: 'found_eg', value: 'value_eg' }
+      ]
+      return results
+    },
+    logout () {
+      console.log('log me out... ')
+    },
+    // Menu toggling methods (including delayed hide on mouse out/in to allow cursor movement from title to dropdown)
+    showMenu (force) {
+      this.visibleMenu = true
+      if (this.waitingToClose) { this.showAfterWait = true }
+    },
+    toggleMenu (force) {
+      this.waitingToClose = false
+      if (this.keepOn) {
+        this.visibleMenu = false
+        this.keepOn = false
+      } else {
+        this.visibleMenu = true
+        this.keepOn = true
+      }
+    },
+    hideMenu (wait) {
+      // Hide menu (delay ignores rapid toggling by mouse out / in movements)
+      var _this = this
+      console.log('hide menu...')
+      if (this.keepOn) {
+        console.log('require direct toggle to turn off')
+      } else if (!wait) {
+        this.visibleMenu = false
+      } else if (this.waitingToClose) {
+        this.showAfterWait = false
+      } else {
+        this.showAfterWait = false
+        setTimeout(
+          () => {
+            _this.waitingToClose = false
+            this.visibleMenu = this.showAfterWait
+          }, 1000)
+        this.waitingToClose = true
+      }
+    },
+    runMethod (opt) {
+      if (opt && opt.onClick) {
+        opt.onClick(opt)
+      }
+
+      if (opt && opt.loadModal) {
+        var results = opt.loadModal(opt)
+
+        this.$store.dispatch('clearModal')
+        // populate modal...
+        this.$store.dispatch('setModalData', results)
+        this.$store.dispatch('toggleModal', 'searchresults')
+      }
+    }
+  }
+}
+</script>
+
+<style scoped lang="sass?outputStyle=expanded">
+
+.option-label {
+  text-align: left
+}
+
+.menu-title {
+  font-size: 2rem;
+}
+
+.customMenu {
+  text-align: right;
+}
+.customMenu a {
+  color: grey;
+}
+.customMenu a:hover {
+  color: black;
+  font-weight: normal;
+  text-decoration: none;
+}
+
+ .popup-table {
+    position: right;
+    right: 0;
+    top: 60px;
+    margin: 0px;
+    background-color: #ddd;
+  }
+  .popup-table tr {
+    /*padding: 5px;*/
+  }
+  .popup-table tr td {
+    /*padding: 0px;*/
+    /*padding-left: 15px;*/
+    border: 1px solid black;
+  }
+
+  .popup-table a{
+    display: block;
+    padding: 5px;
+    padding-left: 15px;
+    /*text-decoration: none;*/
+  }
+  .popup-table a:hover{
+    display: block;
+    padding: 5px;
+    padding-left: 15px;
+    text-decoration: none;
+  }
+</style>
