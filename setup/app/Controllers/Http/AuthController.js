45c6ceb0516e3567771f720c764bdfe346b0a61f
Ran Guin <rguin@iMac.local>
2018-09-17 12:57:30 -0700

    add authController updates

--- setup/app/Controllers/Http/AuthController.js
+++ setup/app/Controllers/Http/AuthController.js
@@ -1,65 +1,93 @@
 'use strict'
 const Hash = use('Hash')
+const User = use('App/Models/User')
+const { validate } = use('Validator')
 
 class AuthController {
-	async login ({auth, request, response}) {
-		console.log('attempt to login with credentials:')
-	    const {email, password} = request.all()
+	async register ({request, response}) {
+	    const rules = {
+	      username: 'required|unique:user,username',
+	      email: 'required|email|unique:user,email',
+	      password: 'required',
+	      confirmPassword: 'required'
+	    }
 
-	    console.log(email + ' : ' + password)
-	    try {
-	    	var loggedIn = auth.check()
-	    	console.log('logged in: ' + JSON.stringify(loggedIn))
-	    	if (loggedIn) {
-		    	console.log('log out first...')
-		    	await auth.logout()
-		    }
-		    console.log('validate: ' + email + ' : ' + password)
+		const validation = await validate(request.all(), rules)
+		if (validation.fails()) {
+			// session
+			// 	.withErrors(validation.messages())
+			// 	.flashExcept(['password'])
+			console.log('Errors: ' + JSON.stringify(validation.messages()))
+	        var msg = {
+    	        errors: 'Failed validation'
+        	}
+			response.json(msg)
+		} else {
+			const user = new User()
 
-			var user = await auth.attempt(email, password)
-			console.log('user: ' + JSON.stringify(user))
-			if (user) {
-				user.password = null
-				var payload = user
-				console.log('regenerated payload: ' + JSON.stringify(payload))
-				response.json(payload)
-			} else {
-				response.json({error: 'failed to authorize'})
-			}
-	    } catch (e) {
-	    	response.unauthorized({error: e.message})
+	        user.username = request.input('username')
+	        user.email = request.input('email')
+	        user.id = null
+	        
+	        const password = request.input('password')
+	        const confirmPassword = request.input('confirmPassword')
+
+	        var verified = await user.encryptPassword(password, confirmPassword)    
+
+	        if (verified) {
+	        	console.log('saving new user: ' + user.username)
+	        	user.save()
+		        var msg = {
+	    	        success: 'Registration Successful! Now go ahead and login'
+	        	}
+	        	response.json(msg)
+	        } else {
+	        	console.log('failed to verify: ' + verified)
+		        var msg = {
+	    	        error: 'Validation Failed'
+	        	}
+	        	response.json(msg)
+	        }
 	    }
 	}
 
-	async loginTest ({ auth, request, response, session }) {
-	    const {email, password} = request.all();
-
-	    try {
-	    	var loggedIn = auth.check()
-
-	    	if (loggedIn) {
-		    	console.log('log out first...')
-		    	await auth.logout()
-		    }
-		    console.log('continue')
-
-		    try {
-				var token = await auth.attempt(email, password)
-
-				if (token) {
-					console.log('token: ' + JSON.stringify(token))
-					var payload = { email: email, status: 'logged in', token: 'xyz'}
-					console.log('regenerated payload: ' + JSON.stringify(payload))
-					response.json(payload)
-				} else {
-					response.json({error: 'failed to authorize'})
-				}
-			} catch (e) {
-				response.json({error: e.message})
-			}
-	    } catch (e) {
-	    	response.unauthorized({error: e.message})
+	async login ({auth, request, response}) {
+		const {email, password} = request.all()
+	    const rules = {
+	      email: 'required|email|unique:user,email',
+	      password: 'required'
 	    }
+
+		console.log('attempt login for ' + email)
+		const validation = await validate(request.all(), rules)
+		if (validation.fails()) {
+			// session
+			// 	.withErrors(validation.messages())
+			// 	.flashExcept(['password'])
+			console.log('Errors: ' + JSON.stringify(validation.messages()))
+	        var msg = {
+    	        errors: 'Failed validation'
+        	}
+			response.json(msg)
+		} else {
+	    	console.log('validate: ' + email + ' : ' + password)
+
+			var user = await auth.attempt(email, password)
+			console.log('user: ' + JSON.stringify(user))
+			response.json(user)
+			// if (user) {
+			// 	user.password = null
+			// 	var payload = user
+			// 	console.log('regenerated payload: ' + JSON.stringify(payload))
+			// 	response.json(payload)
+			// } else {
+	  		//    console.log('failed to verify')
+		 	//        var msg = {
+	  		//   	        error: 'Login Failed'
+	  		//       	}
+	  		//       	response.json(msg)
+			// }
+		}
 	}
 }
 
