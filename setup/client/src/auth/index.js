941b21ab32a8f5c51dc1358a63a6f5cb0b93bcd2
Ran Guin <rguin@iMac.local>
2018-09-17 12:54:31 -0700

    update index file

--- setup/client/src/auth/index.js
+++ setup/client/src/auth/index.js
@@ -1,65 +1,41 @@
 // src/auth/index.js
 
+// import {router} from '../router/'
 import router from '../router/index.js'
-import config from '@/config.js'
+import axios from 'axios'
 
 // URL and endpoint constants
-const API_URL = config.apiUrl
-const LOGIN_URL = API_URL + '/sessions/create/'
-const SIGNUP_URL = API_URL + '/users/'
-const TEST_MODE = true // bypasses login check ...
+const API_URL = 'http://localhost:3333/'
+const LOGIN_URL = API_URL + 'login'
+const SIGNUP_URL = API_URL + 'register/'
 
 export default {
+
   // User object will let us check authentication status
   user: {
     authenticated: false
   },
 
-  // Send a request to the login URL and save the returned JWT
-  login (context, creds, redirect) {
-    if (TEST_MODE) {
-      // Testing login only - until api set up to return tokens
-      localStorage.setItem('id_token', '123')
-      localStorage.setItem('access_token', 'ABC')
-
-      var payload = {user: 'demoUser', userid: '12345', access: 'public'}
-      localStorage.setItem('payload', JSON.stringify(payload))
+  async login (context, creds, redirect) {
+    console.log('auth.index.login ' + LOGIN_URL)
+    // var url = 'http://localhost:3333/register?email=ran.guin@gmail.com&password=shiner'
+    return axios.post(LOGIN_URL, creds, (data) => {
+      localStorage.setItem('id_token', data.id_token)
+      localStorage.setItem('access_token', data.access_token)
 
       this.user.authenticated = true
-      console.log('test mode - Login bypass: ' + JSON.stringify(this.payload()))
-    } else {
-      context.$http.post(LOGIN_URL, creds)
-        .then(data => {
-          localStorage.setItem('id_token', data.id_token)
-          localStorage.setItem('access_token', data.access_token)
-
-          this.user.authenticated = true
-
-          // Redirect to a specified route
-          if (redirect) {
-            router.go(redirect)
-          }
-        }, error => {
-          console.log('Error logging in...')
-          console.log(error)
-        })
-    }
+      console.log('signed up successfully')
+      console.log(JSON.stringify(data))
+      if (redirect) {
+        router.go(redirect)
+      }
+      return data
+    })
   },
-  signup (context, creds, redirect) {
-    context.$http.post(SIGNUP_URL, creds)
-      .then(data => {
-        localStorage.setItem('id_token', data.id_token)
-        localStorage.setItem('access_token', data.access_token)
-
-        this.user.authenticated = true
-
-        if (redirect) {
-          router.go(redirect)
-        }
-      }, error => {
-        console.log('Error signing up ...')
-        context.error(error)
-      })
+  async signup (context, creds, redirect) {
+    console.log('auth.index.signup 2 ' + SIGNUP_URL)
+    // var url = 'http://localhost:3333/register?email=ran.guin@gmail.com&password=shiner'
+    return axios.post(SIGNUP_URL, creds)
   },
 
   // To log out, we just need to remove the token
@@ -71,17 +47,11 @@ export default {
 
   checkAuth () {
     var jwt = localStorage.getItem('id_token')
-
-    this.user.authenticated = false
     if (jwt) {
       this.user.authenticated = true
+    } else {
+      this.user.authenticated = false
     }
-    return this.user.authenticated
-  },
-
-  payload () {
-    var payload = localStorage.getItem('payload')
-    return payload
   },
 
   // The object to be passed as a header for authenticated requests
