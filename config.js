7e6195cc2604b2cc859d0f0a0c1fc9392ba51eac
Ran Guin <ran.guin@gmail.com>
2018-10-12 19:41:41 -0700

    simplify input

--- config.js
+++ config.js
@@ -1,16 +1,12 @@
 export default {
   registrationFields: [
-    {name: 'username', prompt: 'Username'},
-    {name: 'fullname', prompt: 'Full Name'},
-    {name: 'address'},
-    {name: 'postal_code', placeholder: 'postal code', prompt: 'Postal Code'},
-    {name: 'birthdate', type: 'date', placeholder: ' Birthdate (yyyy-mm-dd) '},
+    {name: 'username', type: 'text'},
     {name: 'email', type: 'text'},
     {name: 'password', placeholder: 'password', type: 'password'},
     {name: 'confirmPassword', placeholder: 're-enter password', type: 'password', prompt: '(confirm)'}
   ],
   loginFields: [
-    {name: 'email', type: 'text'},
+    {name: 'email', type: 'text', prompt: 'Email Address'},
     {name: 'password', placeholder: 'password', type: 'password'}
   ]
 }
