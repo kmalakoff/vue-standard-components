ee3ced34a28053ddf2a9c02061fdba7d2e2ea988
Ran Guin <rguin@iMac.local>
2018-10-17 15:09:48 -0700

    standard Test file

---
+++ Test.vue
@@ -0,0 +1,25 @@
+<template lang='pug'>
+  div
+    h1 Test
+    Messaging()
+</template>
+
+<script>
+import Messaging from './Messaging'
+export default {
+  props: {
+  },
+  data () {
+    return {
+    }
+  },
+  components: {
+    Messaging
+  }
+  methods: {
+  }
+}
+</script>
+
+<style scoped>
+</style>
