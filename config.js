4d869b1d4bd06f4727e8551bfbb3194c6568bcc1
Ran Guin <ran.guin@gmail.com>
2018-11-07 18:11:00 -0800

    simplify placeholders

--- config.js
+++ config.js
@@ -2,11 +2,11 @@ export default {
   registrationFields: [
     {name: 'username', type: 'text'},
     {name: 'email', type: 'text'},
-    {name: 'password', placeholder: 'password', type: 'password'},
-    {name: 'confirmPassword', placeholder: 're-enter password', type: 'password', prompt: '(confirm)'}
+    {name: 'password', type: 'password'},
+    {name: 'confirmPassword', placeholder: 're-enter password', type: 'password'}
   ],
   loginFields: [
-    {name: 'email', type: 'text', prompt: 'Email Address'},
-    {name: 'password', placeholder: 'password', type: 'password'}
+    {name: 'email', type: 'text', placeholder: '-- email --', prompt: 'Email Address'},
+    {name: 'password', placeholder: '-- password --', type: 'password', prompt: 'Password'}
   ]
 }
