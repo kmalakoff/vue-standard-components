ec4d7ed3930dda2e91b3e90f60b6d8168c542115
Ran Guin <ran.guin@gmail.com>
2018-11-20 11:12:52 -0800

    update default

--- setup/client/src/auth/index.js
+++ setup/client/src/auth/index.js
@@ -3,11 +3,12 @@
 // import {router} from '../router/'
 // import router from '../router/index.js'
 import axios from 'axios'
+import config from './../config.js'
 
 // URL and endpoint constants
-const API_URL = 'http://localhost:3333/'
-const LOGIN_URL = API_URL + 'login'
-const SIGNUP_URL = API_URL + 'register'
+const API_URL = config.apiURL
+const LOGIN_URL = API_URL + '/login'
+const SIGNUP_URL = API_URL + '/register'
 
 export default {
 
@@ -29,9 +30,10 @@ export default {
     console.log('post auth.index.signup ' + SIGNUP_URL)
     console.log(JSON.stringify(creds))
     // var url = 'http://localhost:3333/register?email=ran.guin@gmail.com&password=shiner'
-    return axios.post(SIGNUP_URL, creds, (data) => {
-      console.log('posted / returned: ' + JSON.stringify(data))
-    })
+    return axios.post(SIGNUP_URL, creds)
+    // , (data) => {
+    //   console.log('posted / returned: ' + JSON.stringify(data))
+    // })
   },
 
   // To log out, we just need to remove the token
