306648cb925fd43f8d42d625ce8d9b51223e2861
Ran Guin <ran.guin@gmail.com>
2018-11-20 20:00:34 -0800

    take out confirm password

--- config.js
+++ config.js
@@ -2,8 +2,8 @@ export default {
   registrationFields: [
     {name: 'username', type: 'text'},
     {name: 'email', type: 'text'},
-    {name: 'password', type: 'password'},
-    {name: 'confirmPassword', placeholder: 're-enter password', type: 'password'}
+    {name: 'password', type: 'password'}
+    // {name: 'confirmPassword', placeholder: 're-enter password', type: 'password'}
   ],
   loginFields: [
     {name: 'email', type: 'text', placeholder: '-- email --', prompt: 'Email Address'},
