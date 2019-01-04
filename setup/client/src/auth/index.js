8c349b1ad72632e954a4295ef80e38fe1e6dbfb0
Ran Guin <ran.guin@gmail.com>
2019-01-04 14:00:53 -0800

    update auth

--- setup/client/src/auth/index.js
+++ setup/client/src/auth/index.js
@@ -7,8 +7,6 @@ import config from './../config.js'
 
 // URL and endpoint constants
 const API_URL = config.apiURL
-const LOGIN_URL = API_URL + '/login'
-const SIGNUP_URL = API_URL + '/register'
 
 export default {
 
@@ -16,41 +14,56 @@ export default {
   user: {
     authenticated: false
   },
-
   // call api to login and return token
   login (context, creds, redirect) {
-    console.log('get auth.index.login ' + LOGIN_URL)
-    console.log(JSON.stringify(creds))
-    var url = LOGIN_URL + '?email=' + creds.email + '&password=' + creds.password
+    var url = API_URL + '/login'
+    url += '?email=' + creds.email + '&password=' + creds.password
+    console.log('get auth.index.login ' + API_URL)
     return axios.get(url, creds)
   },
-
   // call api to register new user
   signup (context, creds, redirect) {
-    console.log('post auth.index.signup ' + SIGNUP_URL)
-    console.log(JSON.stringify(creds))
-    // var url = 'http://localhost:3333/register?email=ran.guin@gmail.com&password=shiner'
-    return axios.post(SIGNUP_URL, creds)
-    // , (data) => {
-    //   console.log('posted / returned: ' + JSON.stringify(data))
-    // })
+    console.log('post auth.index.signup ' + API_URL)
+    var url = API_URL + '/register'
+    url += '?email=' + creds.email
+    url += '&username=' + creds.username
+    url += '&password=' + creds.password
+    url += '&confirmPassword=' + creds.confirmPassword
+
+    // return axios.post(url, creds)
+    return axios.get(url)
+      .catch(function (error) {
+        var msg = '' + error
+        console.log(msg)
+        return {error: msg}
+      })
   },
 
   // To log out, we just need to remove the token
-  logout () {
+  logout (context, loginId) {
     localStorage.removeItem('id_token')
     localStorage.removeItem('access_token')
     this.user.authenticated = false
+
+    var url = API_URL + '/logout'
+    console.log(loginId + ' logout request: ' + url)
+
+    var data = {
+      login_id: loginId
+    }
+    console.log(JSON.stringify(data))
+    return axios.post(url, data)
   },
 
   // just update local authenticated flag
   checkAuth () {
-    var jwt = localStorage.getItem('id_token')
-    if (jwt) {
-      this.user.authenticated = true
-    } else {
-      this.user.authenticated = false
-    }
+    // var jwt = localStorage.getItem('id_token')
+    // if (jwt) {
+    //   this.user.authenticated = true
+    // } else {
+    //   this.user.authenticated = false
+    // }
+    console.log('check authentication... (under construction)')
   },
 
   // The object to be passed as a header for authenticated requests
